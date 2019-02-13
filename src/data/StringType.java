0c1455fcd20d88452bb5f874277b53db6bed1336
Administrator <Administrator@USER-PC>
2019-02-13 12:00:38 +0900

    test

--- src/data/StringType.java
+++ src/data/StringType.java
@@ -4,8 +4,9 @@ public class StringType {
 	public static void main(String[] args) {
 		String str = "123";
 		System.out.println(str + 123);
-		//문자 + 문자 = 문자 , 숫자  + 문자 = 문자 
-
+		// 문자 + 문자 = 문자 , 숫자 + 문자 = 문자
+		String str2 = new String("123");
+		System.out.println(str.contentEquals(str2));
 	}
 
 }
