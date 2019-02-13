ee698337c6948bbf1e50051dcf976957f95d06b0
Administrator <Administrator@USER-PC>
2019-02-13 11:35:37 +0900

    첫 자바프로젝트 깃 연동

---
+++ src/data/Type01.java
@@ -0,0 +1,30 @@
+package data;
+
+public class Type01 {
+	public static void main(String[] args) {
+		// 문자
+		char c = 'a';
+
+		// 숫자 - 정수
+		byte b = 127; // 최대
+		b = -128; // 최소
+
+		short sh = 123;
+
+		int i = 12123123;
+
+		long l = 10000000l;
+
+		// 숫자 - 실수
+		float f = 1.2f;
+
+		double db = 1.2;
+
+		// 논리형(불린)
+		boolean bl = true;
+		boolean bl2 = false;
+
+        //앞글자가 소문자 정해져있는 데이터타입 즉 원시형 데이터타입.
+	}
+
+}
