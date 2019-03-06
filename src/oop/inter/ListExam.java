68847383c33567098e51c511ddcdc5d525282220
lgt8879 <lgt8879@gmail.com>
2019-03-06 10:23:07 +0900

    19.03.05 commit

---
+++ src/oop/inter/ListExam.java
@@ -0,0 +1,15 @@
+package oop.inter;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class ListExam {
+
+	public ArrayList<String> selectArrayList(){
+		return null;
+	}
+	
+	public LinkedList<String> selectLinkedList(){
+		return null;
+	}
+}
---
+++ src/oop/inter/Work.java
@@ -0,0 +1,14 @@
+package oop.inter;
+
+public interface Work {
+	
+	public void goOffice();
+
+	public void working();
+
+	public void goHome();
+
+	public void eat();
+	
+
+}
